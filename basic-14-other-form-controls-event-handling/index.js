78e046fcd5c0a71b322f4d3b2aa953d8a3fd77e2
HelloCodeman <955khm@gmail.com>
2024-04-07 23:14:35 +0800

    Vue 其他表單控件事件處理

---
+++ basic-14-other-form-controls-event-handling/index.js
@@ -0,0 +1,13 @@
+// 示例：其它表單控件處理
+const app = Vue.createApp({
+  data() {
+    return {
+      username: "",
+      gender: "",
+      interests: [],
+      occupation: "",
+      intro:"",
+    }
+  },
+});
+app.mount("#app");
