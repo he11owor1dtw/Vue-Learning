9311c9014822cf017c029198b87c72e88d5ab10e
HelloCodeman <955khm@gmail.com>
2024-04-09 23:03:55 +0800

    Vue v-once 渲染一次

--- 02-show-once/before/index.js
+++
@@ -1,7 +0,0 @@
-// 示例：v-once：只渲染1次 HTML 模板
-const app = Vue.createApp({
-  data() {
-    return {};
-  },
-});
-app.mount("#app");
--- 02-show-once/temp/index.js
+++
@@ -1,9 +0,0 @@
-// 示例：v-once：只渲染1次 HTML 模板
-const app = Vue.createApp({
-  data() {
-    return {
-      list: [1, 2, 3],
-    };
-  },
-});
-app.mount("#app");
