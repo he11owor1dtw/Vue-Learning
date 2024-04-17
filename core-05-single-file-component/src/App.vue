d868f008eba61002d02c05d682996423dc9385e7
HelloCodeman <955khm@gmail.com>
2024-04-17 09:44:59 +0800

    Vue 建立並使用單一文件組件

---
+++ core-05-single-file-component/src/App.vue
@@ -0,0 +1,46 @@
+<script>
+import WordCount from './components/WordCount.vue';
+
+export default {
+  components: { WordCount }
+}
+</script>
+
+<template>
+  <main>
+    <div>
+      <h1>字數統計應用</h1>
+      <WordCount></WordCount>
+    </div>
+  </main>
+</template>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+h1 {
+  margin-bottom: 2em;
+}
+</style>
