e78b04a6a55490fab67df1c3f80b1d8da3440ac3
HelloCodeman <955khm@gmail.com>
2024-04-18 10:51:12 +0800

    Vue 元件動態屬性值綁定

---
+++ core-07-dynamic-props/src/App.vue
@@ -0,0 +1,81 @@
+<template>
+  <main>
+    <div>
+      <MessageItem v-for="message in messages" 
+      :key="message.id" 
+      :sender="'發送者' + message.sender" 
+      :msg="message.msg"
+      :time="message.time" 
+      />
+      <br>  <!-- 可直接 v-bind="message" -->
+        <MessageItem
+        v-for="message in messages"
+        :key="message.id"
+        v-bind="message"
+      />
+    </div>
+  </main>
+</template>
+
+<script>
+import MessageItem from "./components/MessageItem.vue";
+export default {
+  components: {
+    MessageItem,
+  },
+  data() {
+    return {
+      messages: [
+        {
+          id: 1,
+          sender: "张三",
+          msg: "你好呀!",
+          time: "19:38",
+        },
+        {
+          id: 2,
+          sender: "李四",
+          msg: "Hi!",
+          time: "19:40",
+        },
+        {
+          id: 3,
+          sender: "王五",
+          msg: "最近怎么样？",
+          time: "19:42",
+        },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+h1 {
+  margin-bottom: 2em;
+}
+</style>
