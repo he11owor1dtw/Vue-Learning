c47ea0391b1930d45a759577270596ed5932fb1b
HelloCodeman <955khm@gmail.com>
2024-04-19 14:19:48 +0800

    Vue 傳遞組件內未定義的props屬性

---
+++ core-09-non-existed-props/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div>
+      <BlogPostItem v-for="post in posts" :key="post.id" v-bind="post" class="blogLink" />
+    </div>
+  </main>
+</template>
+
+<script>
+import BlogPostItem from "./components/BlogPostItem.vue";
+
+export default {
+  components: {
+    BlogPostItem,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Post 1", link: "https://some.url.com" },
+        { id: 2, title: "Post 2", link: "https://some.url.com" },
+        { id: 3, title: "Post 3", link: "https://some.url.com" },
+      ],
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+.blogLink a{
+  color:bisque;
+}
+</style>
---
+++ core-09-non-existed-props/src/components/BlogPostItem.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <a :href="link">{{ title }}</a>
+  </div>
+</template>
+<script>
+export default {
+  props: ["link", "title"],
+  inheritAttrs:false,     // 設定後會阻止/應用未定義的屬性，如 App.vue 中的 blogLink class
+  mounted(){
+    console.log(this.$attrs.class);
+  }
+};
+</script>
