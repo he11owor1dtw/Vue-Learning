70f7be73a3af5b59a4c574faf301fe50912725ce
HelloCodeman <955khm@gmail.com>
2024-04-19 15:06:49 +0800

    Vue 使用多個 slot 傳遞多個模板到子元件

---
+++ core-11-named-slot/src/App.vue
@@ -0,0 +1,56 @@
+<template>
+  <main>
+    <div>
+      <BaseCard>
+        <template #title>
+          <h2>卡片標題</h2>
+        </template>
+        <template #default>   <!-- 未命名 slot -->
+          <p>这是卡片的内容</p>
+          <p>可以插入任何 HTML 模板</p>
+          <a href="#">这是一个超链接</a>
+        </template>
+      </BaseCard>
+    </div>
+  </main>
+</template>
+
+<script>
+import BaseCard from "./components/BaseCard.vue";
+
+export default {
+  components: {
+    BaseCard,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+a {
+  color: hsl(250deg, 100%, 90%);
+}
+</style>
---
+++ core-11-named-slot/src/components/BaseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="title">
+      <slot name="title"></slot>
+    </div>
+    <div class="content">
+      <slot></slot>  <!-- 未命名插槽，默認屬性為 #default -->
+    </div>
+  </div>
+</template>
+<script>
+export default {};
+</script>
+<style scoped>
+.card {
+  border: 1px solid hsl(250deg, 100%, 80%);
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.title {
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-bottom: 12px;
+}
+</style>
