1c8df416b4629b952e6283ac4b0cec43f4f629d2
HelloCodeman <955khm@gmail.com>
2024-04-19 14:42:15 +0800

    Vue 插槽slot：傳遞範本到子元件

---
+++ core-10-slot-intro/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <div>
+      <BaseCard title="卡片容器">
+        <!-- <p>這是卡片的内容</p>
+        <p>可以插入任何 HTML 模板</p>
+        <a href="#">這是一個連結</a> -->
+      </BaseCard>
+    </div>
+  </main>
+</template>
+
+<script>
+import BaseCard from "./components/BaseCard.vue";
+
+export default {
+  components: {
+    BaseCard,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+a {
+  color: hsl(250deg, 100%, 90%);
+}
+</style>
---
+++ core-10-slot-intro/src/components/BaseCard.vue
@@ -0,0 +1,26 @@
+<template>
+  <div class="card">
+    <div class="title">{{ title }}</div>
+    <div class="content">
+      <slot>默認內容</slot>   <!-- 直接輸入也可顯示 -->
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: ["title"],
+};
+</script>
+<style scoped>
+.card {
+  border: 1px solid hsl(250deg, 100%, 80%);
+  padding: 20px;
+  border-radius: 8px;
+}
+
+.title {
+  font-size: 1.5em;
+  font-weight: bold;
+  margin-bottom: 12px;
+}
+</style>
