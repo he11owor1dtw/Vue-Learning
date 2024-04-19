39cc56a77bdbc17715cc817506f489af9c902b7e
HelloCodeman <955khm@gmail.com>
2024-04-19 15:29:19 +0800

    Vue 子元件事件：向父元件傳遞資料的途徑

---
+++ core-12-events-intro/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <div>
+      <BlogPostItem v-for="post in posts" 
+      :key="post.id" 
+      v-bind="post" 
+      @deletePost="handleDeletePost" />
+    </div>
+  </main>
+</template>
+
+<script>
+import BlogPostItem from "./components/BlogPostItem.vue";
+
+export default {
+  components: {
+    BlogPostItem,
+  },
+  data() {
+    return {
+      posts: [
+        { id: 1, title: "Post 1", link: "https://some.url.com" },
+        { id: 2, title: "Post 2", link: "https://some.url.com" },
+        { id: 3, title: "Post 3", link: "https://some.url.com" },
+      ],
+    };
+  },
+  methods: {
+    handleDeletePost(id) {
+      this.posts = this.posts.filter(p => p.id !== id);
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+a {
+  color: hsl(250deg, 100%, 90%);
+  text-decoration: none;
+}
+</style>
---
+++ core-12-events-intro/src/components/BlogPostItem.vue
@@ -0,0 +1,28 @@
+<template>
+  <div>
+    <a :href="link">{{ title }}</a>
+    <button @click="$emit('deletePost', id)">刪除</button>  <!-- $emit 傳遞回子組件 -->
+  </div>
+</template>
+<script>
+export default {
+  props: ["id", "link", "title"],
+  emits: ["deletePost"],
+};
+</script>
+<style scoped>
+div {
+  margin-bottom: 1em;
+}
+
+button {
+  border: none;
+  background: linear-gradient(90deg,
+      hsl(240deg, 50%, 50%),
+      hsl(280deg, 50%, 50%));
+  padding: 3px 4px;
+  margin-left: 12px;
+  border-radius: 4px;
+  color: white;
+}
+</style>
