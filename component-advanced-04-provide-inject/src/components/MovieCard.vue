81ae5dab621d015aa610bbf24d99ea63376cfc0e
HelloCodeman <955khm@gmail.com>
2024-04-21 15:45:50 +0800

    Vue provide/inject 資料多層傳遞

---
+++ component-advanced-04-provide-inject/src/components/MovieCard.vue
@@ -0,0 +1,32 @@
+<template>
+  <div class="card">
+    <MovieItem :description="movie.description" />
+  </div>
+</template>
+<script>
+import MovieItem from "./MovieItem.vue";
+export default {
+  components: {
+    MovieItem,
+  },
+  data() {
+    return {
+      movie: {
+        title: "電影",
+        description: "這是一段電影的描述",
+      },
+    };
+  },
+  // provide/inject 解決資料多層傳遞的問題
+  provide() {   // 須定義為一個函數，像 data 一樣
+    return { title: this.movie.title };  // 非響應式的呈現 data 數據
+  },
+};
+</script>
+<style scoped>
+.card {
+  padding: 12px;
+  border: 1px solid hsl(240deg, 100%, 80%);
+  border-radius: 4px;
+}
+</style>
---
+++ component-advanced-04-provide-inject/src/components/MovieTitle.vue
@@ -0,0 +1,13 @@
+<template>
+  <h2>{{ title }}</h2>
+</template>
+<script>
+export default {
+  inject: ["title"]
+};
+</script>
+<style scoped>
+h2 {
+  color: hsl(240deg, 100%, 80%);
+}
+</style>
