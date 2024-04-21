e8431e10c79dfbd97d34dcee5e85903c4492742b
HelloCodeman <955khm@gmail.com>
2024-04-21 16:00:31 +0800

    Vue 在傳遞的 slot 中，訪問子元件的資料

---
+++ component-advanced-05-scoped-slots/src/App.vue
@@ -0,0 +1,52 @@
+<template>
+  <main>
+    <div>
+      <ContactList v-slot="{ contact }"> <!-- 若標籤內有多個 slot，不能使用解構語法 -->
+        <!-- <template v-slot:default="props"> 由於標籤內只有一個 slot，可省略 template 標籤 -->
+        <!-- <ContactList v-slot:default="props"> 由於標籤內只有一個 slot，:default 可省略 -->
+        <!-- <ContactList v-slot="props"> -->
+        <!-- <p>{{ props.contact.name }}</p>
+        <p>{{ props.contact.email }}</p> -->
+        <p>{{ contact.name }}</p>
+        <p>{{ contact.email }}</p>
+        <!-- </template> -->
+      </ContactList>
+    </div>
+  </main>
+</template>
+
+<script>
+import ContactList from "./components/ContactList.vue";
+
+export default {
+  components: {
+    ContactList,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ component-advanced-05-scoped-slots/src/components/ContactList.vue
@@ -0,0 +1,40 @@
+<template>
+  <ul>
+    <li v-for="contact in contacts" :key="contact.id">
+      <slot :contact="contact"/>
+    </li>
+  </ul>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      contacts: [
+        {
+          id: 1,
+          name: "John Doe",
+          email: "johndoe@domain.com",
+        },
+        {
+          id: 2,
+          name: "Karen Williams",
+          email: "kwill@domai.com",
+        },
+        {
+          id: 3,
+          name: "Steve Rogers",
+          email: "stevrog@domain.com",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+ul {
+  list-style: none;
+}
+ul li {
+  padding: 1em 0;
+}
+</style>
