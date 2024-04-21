8844c6fce4040ac5f0225fdfa67dc9fa75489ad4
HelloCodeman <955khm@gmail.com>
2024-04-21 15:26:10 +0800

    Vue 父子元件的數據流向設計

---
+++ component-advanced-03-props-down-events-up/src/App.vue
@@ -0,0 +1,53 @@
+<template>
+  <main>
+    <div>
+      <ButtonWithProps :onclick="handleClick" />
+      <ButtonWithEmits @customClick="handleClick" />
+      <ButtonWithProps /> <!-- F12 會報錯，提示該值非一個函數 undefined，因沒有傳遞任何數據-->
+      <ButtonWithEmits /> <!-- 無報錯，因使用 $emit，不依賴父元件傳遞的事件處理函數-->
+    </div>
+  </main>
+</template>
+
+<script>
+import ButtonWithProps from "./components/ButtonWithProps.vue";
+import ButtonWithEmits from "./components/ButtonWithEmits.vue";
+
+export default {
+  components: {
+    ButtonWithProps,
+    ButtonWithEmits,
+  },
+  methods: {
+    handleClick(value) {
+      alert(value);
+    },
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ component-advanced-03-props-down-events-up/src/components/ButtonWithProps.vue
@@ -0,0 +1,23 @@
+<template>
+  <button @click="onclick('點擊了按鈕')">按鈕</button>
+</template>
+<script>
+export default {
+  props: ["onclick"],  // 值為一個事件處理函數
+};
+</script>
+<style scoped>
+button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 1em 2em;
+  margin-top: 24px;
+  margin-right: 12px;
+  border-radius: 4px;
+  color: white;
+}
+</style>
