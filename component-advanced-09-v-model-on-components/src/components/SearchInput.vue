ed27ae69696e7b04a6532e4ecc26f41042d4e45b
HelloCodeman <955khm@gmail.com>
2024-04-23 11:02:47 +0800

    Vue 讓元件支持 v-model 指令

---
+++ component-advanced-09-v-model-on-components/src/components/SearchInput.vue
@@ -0,0 +1,22 @@
+<template>
+  <label>
+    <span>查詢：</span>
+    <input type="text" :value="modelValue" @input="$emit('update:modelValue', $event.target.value)" />
+  </label>
+</template>
+<script>
+export default {
+  props: ["modelValue"],
+  emits: ["update:modelValue"],
+};
+</script>
+<style scoped>
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+</style>
