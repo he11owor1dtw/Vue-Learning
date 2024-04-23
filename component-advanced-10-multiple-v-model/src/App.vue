cb0739c355c44e411461a6c7575b18d3172e1079
HelloCodeman <955khm@gmail.com>
2024-04-23 11:23:38 +0800

    Vue 使用多個 v-model 指令

---
+++ component-advanced-10-multiple-v-model/src/App.vue
@@ -0,0 +1,62 @@
+<template>
+  <main>
+    <div>
+      <!-- v-model 後面的參數必須和子元件接收的屬性名相同，例如 searchTerm -->
+      <SearchInput 
+      v-model:searchTerm="searchTerm" 
+      v-model:category="category" 
+      />
+      <hr>
+      <p>查詢字：{{ searchTerm }}</p>
+      <p>類別：{{ category }}</p>
+    </div>
+  </main>
+</template>
+
+<script>
+import SearchInput from "./components/SearchInput.vue";
+
+export default {
+  components: {
+    SearchInput,
+  },
+  data() {
+    return {
+      // 名字無需和 SearchInput 中的屬性名相同
+      searchTerm: "",
+      category: "default",
+    };
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+p {
+  margin-top: 12px;
+  color: hsl(50deg, 100%, 80%);
+}
+</style>
---
+++ component-advanced-10-multiple-v-model/src/components/SearchInput.vue
@@ -0,0 +1,48 @@
+<template>
+  <label>
+    <span>查詢：</span>
+    <input type="text" 
+    :value="searchTerm" 
+    @input="$emit('update:searchTerm', $event.target.value)" />
+  </label>
+  <label>
+    <span>類別：</span>
+    <select 
+    :value="category" 
+    @change="$emit('update:category', $event.target.value)">
+      <option value="default">預設</option>
+      <option value="fontend">前端</option>
+      <option value="backend">後端</option>
+      <option value="fullstack">全端</option>
+    </select>
+  </label>
+</template>
+<script>
+export default {
+  props: ["searchTerm", "category"],
+  emits: ["update:searchTerm", "update:category"],
+};
+</script>
+<style scoped>
+label {
+  display: block;
+  margin-bottom: 20px;
+}
+
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+
+select {
+  border: 1px solid hsl(280deg, 50%, 50%);
+  background: none;
+  color: white;
+  padding: 6px;
+  border-radius: 4px;
+}
+</style>
