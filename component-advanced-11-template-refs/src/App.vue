10c5d62671aba1fd4f79af6fc434ed862bbeea38
HelloCodeman <955khm@gmail.com>
2024-04-24 15:45:35 +0800

    Vue Refs：獲取 DOM 和子元件範例

---
+++ component-advanced-11-template-refs/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <div>
+      <AutoFocus ref="autofocus"/>
+    </div>
+  </main>
+</template>
+
+<script>
+import AutoFocus from "./components/AutoFocus.vue";
+export default {
+  components: {
+    AutoFocus,
+  },
+  mounted(){
+    setTimeout(()=>{
+      console.log(this.$refs.autofocus.inputText);
+      this.$refs.autofocus.blur();
+    },5000);
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ component-advanced-11-template-refs/src/components/AutoFocus.vue
@@ -0,0 +1,32 @@
+<template>
+  <input type="text" v-model="inputText" ref="inputControl" />
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      inputText: ""
+    };
+  },
+  methods: {
+    blur() {
+      this.$refs.inputControl.blur();
+    },
+  },
+  mounted() {
+    this.$refs.inputControl.focus();
+  }
+};
+</script>
+
+<style scoped>
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+</style>
