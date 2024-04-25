daa97d87d2e584dfc1c86b5938f6eae729e00996
HelloCodeman <955khm@gmail.com>
2024-04-25 17:57:02 +0800

    Vue 利用<teleport>傳送元件：多次傳送

---
+++ component-advanced-14-02-teleport-multiple-times/src/App.vue
@@ -0,0 +1,73 @@
+<template>
+  <main>
+    <div class="container">
+      <button @click="msgs.push(`這是一段消息${msgs.length + 1}`)">增加消息</button>
+      <AlertBox v-for="msg in msgs">{{ msg }}</AlertBox>
+    </div>
+  </main>
+</template>
+
+<script>
+import AlertBox from "./components/AlertBox.vue";
+export default {
+  components: {
+    AlertBox,
+  },
+  data() {
+    return {
+      msgs: [],
+    }
+  }
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(#212943 0.6000000000000001px,
+      #0f141c 0.6000000000000001px);
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+button {
+  border: none;
+  background: linear-gradient(90deg,
+      hsl(240deg, 50%, 50%),
+      hsl(280deg, 50%, 50%));
+  padding: 12px 18px;
+  margin-top: 12px;
+  border-radius: 4px;
+  color: white;
+}
+
+.container {
+  position: relative;
+  /* border: 1px solid hsl(280deg, 100%, 50%); */
+}
+
+#messages {
+  position: absolute;
+  right: 12px;
+  bottom: 12px;
+  display: flex;
+  flex-direction: column-reverse;
+  gap: 12px;
+}
+</style>
---
+++ component-advanced-14-02-teleport-multiple-times/src/components/AlertBox.vue
@@ -0,0 +1,50 @@
+<template>
+  <Teleport to="#messages">
+    <div v-if="show" class="alertBox">
+      <div class="closeIcon" @click="show = false">X</div>
+      <div class="content">
+        <slot>消息提示框元件</slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+  // 3 秒後讓消息框自動消失
+  mounted() {
+    setTimeout(() => {
+      this.show = false
+    }, 3000)
+  }
+};
+</script>
+<style scoped>
+.alertBox {
+  width: 350px;
+  height: 80px;
+  border: 1px solid hsl(280, 100%, 50%);
+  border-radius: 8px;
+  padding: 24px;
+  position: relative;
+}
+
+.content {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.closeIcon {
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  font-size: 12px;
+  font-weight: 900;
+  cursor: pointer;
+}
+</style>
