8e5c11ac8b3ff152ea46ff8c0cbae466c706f02c
HelloCodeman <955khm@gmail.com>
2024-04-25 10:53:02 +0800

    Vue 使用動態元件渲染不同的 HTML 標籤

---
+++ component-advanced-13-01-dynamic-components/src/App.vue
@@ -0,0 +1,50 @@
+<template>
+  <main>
+    <div>
+      <!-- 動態 HTML 元素  -->
+      <TextHeading level="1">一級標題</TextHeading>
+      <TextHeading level="2">二級標題</TextHeading>
+      <TextHeading level="3">三級標題</TextHeading>
+      <TextHeading level="4">四級標題</TextHeading>
+      <TextHeading level="5">五級標題</TextHeading>
+      <TextHeading level="6">六級標題</TextHeading>
+    </div>
+  </main>
+</template>
+
+<script>
+import TextHeading from "./components/TextHeading.vue";
+export default {
+  components: {
+    TextHeading,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ component-advanced-13-01-dynamic-components/src/components/TextHeading.vue
@@ -0,0 +1,35 @@
+<template>
+  <Component :is="heading"><slot></slot></Component>
+</template>
+
+<script>
+export default {
+  props: ["level"],
+  computed: {
+    heading() {
+      return `h${this.level}`;
+    },
+  },
+};
+</script>
+
+<style scoped>
+h1 {
+  font-size: 3em;
+}
+h2 {
+  font-size: 2.4em;
+}
+h3 {
+  font-size: 1.8em;
+}
+h4 {
+  font-size: 1.4em;
+}
+h5 {
+  font-size: 1.2em;
+}
+h6 {
+  font-size: 1em;
+}
+</style>
