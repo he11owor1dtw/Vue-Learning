843a8d814b3247e799387e5b7375b669f626cb66
HelloCodeman <955khm@gmail.com>
2024-04-25 17:45:30 +0800

    Vue 利用<teleport>傳送元件：在其他 DOM 元件掛載元件

---
+++ component-advanced-14-01-teleport-render-template-outside-of-component/src/components/AlertBox.vue
@@ -0,0 +1,46 @@
+<template>
+  <Teleport to="body">
+    <div v-if="show" class="alertBox">
+      <div class="closeIcon" @click="show = false">X</div>
+      <div class="content">
+        <slot>消息提示框元件</slot>
+      </div>
+    </div>
+  </Teleport>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      show: true,
+    };
+  },
+};
+</script>
+<style scoped>
+.alertBox {
+  width: 350px;
+  height: 80px;
+  border: 1px solid hsl(280, 100%, 50%);
+  border-radius: 8px;
+  padding: 24px;
+  position: absolute;
+  right: 12px;
+  bottom: 12px;
+}
+
+.content {
+  height: 100%;
+  display: flex;
+  align-items: center;
+}
+
+.closeIcon {
+  position: absolute;
+  right: 12px;
+  top: 12px;
+  font-size: 12px;
+  font-weight: 900;
+  cursor: pointer;
+}
+</style>
