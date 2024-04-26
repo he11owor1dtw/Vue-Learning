03e83e71102f351af488bc7aa91166c15678a19b
HelloCodeman <955khm@gmail.com>
2024-04-26 18:32:32 +0800

    Vue 渲染函数 h() & JSX

---
+++ component-advanced-15-01-render-programatically-template/src/components/BaseCard.vue
@@ -0,0 +1,32 @@
+<script>
+import { h } from "vue";
+
+export default {
+  props: ["title"],
+  render() {
+    return h("div", { class: "card" }, [
+      h("div", { class: "title" }, this.title),
+      h("div", { class: "content" }, this.$slots.default()),  
+      // 第三節點為 slot，需使用 this.$slots.default()，只有一個預設 slot 使用 default()
+    ]);
+  },
+};
+</script>
+<style scoped>
+.card {
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 8px;
+  padding: 12px;
+  min-width: 240px;
+}
+
+.title {
+  border-bottom: 2px solid hsl(280deg, 50%, 50%);
+  padding-bottom: 8px;
+}
+
+.content {
+  min-height: 20px;
+  margin-top: 18px;
+}
+</style>
