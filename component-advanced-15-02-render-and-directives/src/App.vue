42b2e2e261e269cfdf0b8f76672fba642f86d314
HelloCodeman <955khm@gmail.com>
2024-04-26 18:41:19 +0800

    Vue 在渲染函数中使用指令

---
+++ component-advanced-15-02-render-and-directives/src/App.vue
@@ -0,0 +1,57 @@
+<template>
+  <main>
+    <div>
+      <BaseCard title="卡片標題">這是卡片內容</BaseCard>
+    </div>
+  </main>
+</template>
+
+<script>
+import BaseCard from "./components/BaseCard.vue";
+export default {
+  components: {
+    BaseCard,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 12px 18px;
+  margin-top: 12px;
+  border-radius: 4px;
+  color: white;
+}
+</style>
---
+++ component-advanced-15-02-render-and-directives/src/components/BaseCard.vue
@@ -0,0 +1,35 @@
+<script>
+import { h } from "vue";
+
+export default {
+  props: ["title"],
+  render() {
+    return h("div", { class: "card" }, [
+      h("div", { class: "title" }, this.title),
+      h("div", { class: "content" }, this.$slots.default()),
+      [1, 2, 3, 4].map((item) => h("h" + item, {}, item)),  
+      // 用 map 方法遍歷陣列，第一個參數用 "h" 顯示成 h1-h4
+      // 第二個參數不傳值，留空
+      // 第三個參數為遍歷出來的內容作為子節點
+    ]);
+  },
+};
+</script>
+<style scoped>
+.card {
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 8px;
+  padding: 12px;
+  min-width: 240px;
+}
+
+.title {
+  border-bottom: 2px solid hsl(280deg, 50%, 50%);
+  padding-bottom: 8px;
+}
+
+.content {
+  min-height: 20px;
+  margin-top: 18px;
+}
+</style>
