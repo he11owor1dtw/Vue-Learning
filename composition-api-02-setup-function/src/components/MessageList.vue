b93b1fc453724f1275a1a4ee9222f2f9d8ac4bdd
HelloCodeman <955khm@gmail.com>
2024-04-29 22:20:32 +0800

    Vue setup() 函數：Composition API 的入口

---
+++ composition-api-02-setup-function/src/components/MessageList.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul>
+    <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
+  </ul>
+</template>
+<script>
+export default {
+  setup() {
+    const messages = [
+      { id: 1, content: "這是一條消息提醒1" },
+      { id: 2, content: "這是一條消息提醒2" },
+      { id: 3, content: "這是一條消息提醒3" },
+      { id: 4, content: "這是一條消息提醒4" },
+    ];
+
+    return { messages };
+  },
+};
+</script>
+<style scoped>
+h2 {
+  color: hsl(280deg, 100%, 70%);
+  margin-bottom: 24px;
+  text-align: center;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
