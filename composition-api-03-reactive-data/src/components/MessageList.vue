15094a80ba35bc2a77ca6edac23372e43f4fd06c
HelloCodeman <955khm@gmail.com>
2024-04-30 14:23:24 +0800

    Vue ref() 函數 定義響應式數據

---
+++ composition-api-03-reactive-data/src/components/MessageList.vue
@@ -0,0 +1,51 @@
+<template>
+  <div>
+    <ul>
+      <li v-for="msg in messages" :key="msg.id">{{ msg.content }}</li>
+    </ul>
+    <button @click="messages = []">刪除全部</button>
+  </div>
+</template>
+<script>
+import { ref } from 'vue';
+
+export default {
+  setup() {
+    const messages = ref([
+      { id: 1, content: "這是一條消息提醒1" },
+      { id: 2, content: "這是一條消息提醒2" },
+      { id: 3, content: "這是一條消息提醒3" },
+      { id: 4, content: "這是一條消息提醒4" },
+    ]);
+    console.log(messages.value);
+    return { messages };
+  },
+};
+</script>
+<style scoped>
+div {
+  display: grid;
+  place-items: center;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
