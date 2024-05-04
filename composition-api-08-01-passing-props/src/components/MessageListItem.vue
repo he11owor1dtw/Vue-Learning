a12de518e2d2916cb2a6efce277b35bca6551f7d
HelloCodeman <955khm@gmail.com>
2024-05-04 22:06:50 +0800

    Vue 傳遞和訪問 Props 屬性

---
+++ composition-api-08-01-passing-props/src/components/MessageListItem.vue
@@ -0,0 +1,31 @@
+<template>
+  <li>{{ msg }}</li>
+</template>
+<script>
+import { ref, watch, watchEffect, toRefs } from "vue";
+
+export default {
+  props: ["msg"],
+  setup(props) {
+    console.log(props.msg);
+
+    return {};
+  },
+};
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
