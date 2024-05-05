c2dee5a7d2da3d68a474615202983e125af3f14a
HelloCodeman <955khm@gmail.com>
2024-05-05 21:51:16 +0800

    Vue  轉換非響應性 Props 為響應性

---
+++ composition-api-08-02-props-reactivity/src/components/MessageList.vue
@@ -0,0 +1,66 @@
+<template>
+  <div>
+    <ul>
+      <MessageListItem v-for="msg in messages" :key="msg.id" :msg="msg.content"></MessageListItem>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, watch, watchEffect } from "vue";
+import MessageListItem from "./MessageListItem.vue";
+
+export default {
+  components: { MessageListItem },
+  setup(props) {
+    const messages = ref([
+      { id: 1, content: "這是一條消息提醒1" },
+      { id: 2, content: "這是一條消息提醒2" },
+      { id: 3, content: "這是一條消息提醒3" },
+      { id: 4, content: "這是一條消息提醒4" },
+    ]);
+
+    setTimeout(() => {
+      // messages 是一個 ref()，所以需要透過 .value 去訪問值
+      messages.value[1].content = "這是一條消息提醒2-修改"
+      }, 1500)
+
+      return { messages };
+    },
+};
+</script>
+<style scoped>
+div {
+  display: grid;
+  place-items: center;
+  gap: 24px;
+}
+
+h2 {
+  color: hsl(280deg, 50%, 70%);
+}
+
+p {
+  margin: 12px 0 24px 0;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
---
+++ composition-api-08-02-props-reactivity/src/components/MessageListItem.vue
@@ -0,0 +1,42 @@
+<template>
+  <li>{{ msg }}</li>
+</template>
+<script>
+import { ref, watch, watchEffect, toRefs } from "vue";
+
+export default {
+  props: ["msg"],
+  setup(props) {
+    // console.log(props.msg);
+    // console.log(props);
+
+    const { msg } = toRefs(props);  // 將原本非響應性的 props 轉換為響應性
+
+    // watch(() => props.msg, (newMsg) => {
+    //   console.log(newMsg);
+    // })
+    // return {};
+
+    watch(msg, (newMsg) => {
+      console.log(newMsg);
+    })
+    return {};
+  },
+};
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
