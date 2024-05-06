613ba65cf97ad2e95650c24f2968b73b411b0cfb
HelloCodeman <955khm@gmail.com>
2024-05-06 15:06:02 +0800

    Vue ref/reactive 創建的數據在 Props 中的響應性

---
+++ composition-api-08-03-passing-reactive-props/src/components/MessageList.vue
@@ -0,0 +1,70 @@
+<template>
+  <div>
+    <ul>
+      <MessageListItem v-for="msg in messages" :key="msg.id" :msg="msg.content"></MessageListItem>
+    </ul>
+  </div>
+</template>
+<script>
+import { ref, watch, watchEffect } from "vue";
+import MessageListItem from "./MessageListItem.vue";
+
+export default {
+  props: ["messages"],
+  setup(props) {
+    console.log(props.messages);
+
+    // 這裡 messages 是 ref 中的 value
+
+    // const { messages } = props;
+
+    watch(
+      () => props.messages, (newMessages) => {
+        console.log(newMessages.length);
+      },
+      {
+        deep: true,
+      }
+    );
+
+    return {};
+  },
+  components: { MessageListItem },
+};
+</script>
+<style scoped>
+div {
+  display: grid;
+  place-items: center;
+  gap: 24px;
+}
+
+h2 {
+  color: hsl(280deg, 50%, 70%);
+}
+
+p {
+  margin: 12px 0 24px 0;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
---
+++ composition-api-08-03-passing-reactive-props/src/components/MessageListItem.vue
@@ -0,0 +1,24 @@
+<template>
+  <li>{{ msg }}</li>
+</template>
+<script>
+export default {
+  props: ["msg"],
+};
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
