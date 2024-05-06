6eafe5d0a0031b23ed54da35c275ef24f0fb7e6e
HelloCodeman <955khm@gmail.com>
2024-05-06 15:12:43 +0800

    Vue 在 setup() 中定義 methods

---
+++ composition-api-09-methods/src/components/MessageListItem.vue
@@ -0,0 +1,32 @@
+<template>
+  <li>{{ msg }}<button @click="removeMessage(id)">刪除</button></li>
+</template>
+<script>
+import { ref, watch, watchEffect, toRefs } from "vue";
+
+export default {
+  props: ["msg", "id"],
+  setup(props) {
+    function removeMessage(id) {
+      console.log("刪除消息",id);
+    }
+    return { removeMessage }
+  },
+};
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
