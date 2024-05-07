43da4b4ea40cf94bc77dfd9416376a0ee8f69487
HelloCodeman <955khm@gmail.com>
2024-05-07 16:55:22 +0800

    Vue Emit 自定義事件

---
+++ composition-api-10-emit-events/src/App.vue
@@ -0,0 +1,58 @@
+<template>
+  <main>
+    <div class="container">
+      <MessageList />
+    </div>
+  </main>
+</template>
+
+<script>
+import MessageList from "./components/MessageList.vue";
+
+export default {
+  components: {
+    MessageList,
+  },
+  setup() {},
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+button {
+  border: none;
+  background: linear-gradient(
+    90deg,
+    hsl(240deg, 50%, 50%),
+    hsl(280deg, 50%, 50%)
+  );
+  padding: 0.5em 1.4em;
+  border-radius: 4px;
+  color: white;
+}
+</style>
---
+++ composition-api-10-emit-events/src/components/MessageListItem.vue
@@ -0,0 +1,34 @@
+<template>
+  <!-- <li>{{ msg }} <button @click="removeMessage(id)">刪除</button></li> -->
+  {{ msg }} <button @click="$emit('remove', id)">删除</button>
+</template>
+<script>
+import { ref, watch, watchEffect, toRefs } from "vue";
+
+export default {
+  props: ["msg", "id"],
+  emits: ["remove"],
+  setup(props, { emit }) {
+    // function removeMessage(id) {
+    //   emit("remove", id)
+    // }
+    // return { removeMessage };
+  },
+};
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
