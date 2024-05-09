bcef186f5f3791741dd292f5595c076b963384df
HelloCodeman <955khm@gmail.com>
2024-05-09 10:54:27 +0800

    Vue 渲染函數：使用 context 中的 slots 屬性

---
+++ composition-api-15-slots/src/App.vue
@@ -0,0 +1,65 @@
+<template>
+  <main>
+    <div>
+      <BaseLayout>
+        <template v-slot:header>
+          <nav>
+            <a href="#">首頁</a>
+            <a href="#">關於</a>
+            <a href="#">聯絡我</a>
+          </nav>
+        </template>
+        <template v-slot:default>
+          <p>這是內容</p>
+        </template>
+      </BaseLayout>
+    </div>
+  </main>
+</template>
+
+<script>
+import BaseLayout from "./components/BaseLayout.vue";
+export default {
+  components: {
+    BaseLayout,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+nav {
+  display: flex;
+  gap: 24px;
+  margin-bottom: 36px;
+}
+
+nav a {
+  color: hsl(280deg, 100%, 80%);
+}
+</style>
---
+++ composition-api-15-slots/src/components/BaseLayout.vue
@@ -0,0 +1,23 @@
+<template>
+  <header>
+    <slot name="header"></slot>
+  </header>
+  <main>
+    <slot></slot>
+  </main>
+</template>
+
+<script>
+import { h } from "vue";
+
+export default {
+  setup(props, { slots }) {
+    return () => [
+      h("header", {}, slots.header()),
+      h("main", {}, slots.default()),
+    ]
+  },
+};
+</script>
+
+<style scoped></style>
