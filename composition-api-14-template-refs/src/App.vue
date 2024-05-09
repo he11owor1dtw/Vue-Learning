d71251f6959f659bba680aea523c9822504b972a
HelloCodeman <955khm@gmail.com>
2024-05-09 10:42:06 +0800

    Vue 取得 template ref

---
+++ composition-api-14-template-refs/src/App.vue
@@ -0,0 +1,44 @@
+<template>
+  <main>
+    <div>
+      <AutoFocus />
+    </div>
+  </main>
+</template>
+
+<script>
+import AutoFocus from "./components/AutoFocus.vue";
+export default {
+  components: {
+    AutoFocus,
+  },
+};
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ composition-api-14-template-refs/src/components/AutoFocus.vue
@@ -0,0 +1,31 @@
+<template>
+  <input type="text" v-model="inputText" ref="inputControl" />
+</template>
+
+<script>
+import { onMounted,ref } from 'vue';
+
+export default {
+  setup() {
+    const inputText = ref("");
+    const inputControl = ref(null);
+
+    onMounted(() => {
+      inputControl.value.focus();
+    });
+
+    return { inputText, inputControl };
+  },
+};
+</script>
+
+<style scoped>
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+</style>
