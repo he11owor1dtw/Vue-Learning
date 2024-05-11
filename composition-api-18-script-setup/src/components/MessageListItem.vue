05df04df3d763a8cec1c8c4010cc6c71fa546e86
HelloCodeman <955khm@gmail.com>
2024-05-11 21:53:09 +0800

    Vue script setup: 簡化元件代碼

---
+++ composition-api-18-script-setup/src/components/MessageListItem.vue
@@ -0,0 +1,37 @@
+<template>
+  <li>
+    {{ msg }} <button @click="$emit('remove', id)">删除</button>
+  </li>
+</template>
+<script setup>
+import { ref, watch, watchEffect, toRefs } from "vue";
+
+// 於 <script> 內增加 setup 並利用 defineProps, defineEmits 以簡化代碼
+defineProps(["msg", "id"]);
+defineEmits(["remove"]);
+
+// export default {
+//   props: ["msg", "id"],
+//   emits: ["remove"],
+//   setup(props) {
+//     return {};
+//   },
+// };
+
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
