ebfdbc70a19daa0738ee1874d82bec58c6f84364
HelloCodeman <955khm@gmail.com>
2024-05-12 02:38:21 +0800

    Vue defineModel：簡化的元件 v-model 綁定

---
+++ composition-api-19-01-how-to-use-define-model/src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <main>
+    <div>
+      <SearchInput v-model="searchTerm" />
+      <p>{{ searchTerm }}</p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import SearchInput from "./components/SearchInput.vue";
+
+const searchTerm = ref("");
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+
+p {
+  margin-top: 12px;
+}
+</style>
---
+++ composition-api-19-01-how-to-use-define-model/src/components/SearchInput.vue
@@ -0,0 +1,19 @@
+<template>
+  <label>
+    <span>搜尋：</span>
+    <input type="text" v-model="searchModel" />
+  </label>
+</template>
+<script setup>
+const searchModel = defineModel();
+</script>
+<style scoped>
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+</style>
