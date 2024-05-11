eb020605cf84f1fa71de0a6c07320e5e9feaab04
HelloCodeman <955khm@gmail.com>
2024-05-12 02:47:24 +0800

    Vue defineModel 和多個 v-model 绑定

---
+++ composition-api-19-02-multiple-models/src/App.vue
@@ -0,0 +1,49 @@
+<template>
+  <main>
+    <div>
+      <SearchInput
+        v-model:searchModel="searchTerm"
+        v-model:categoryModel="category"
+      />
+      <div class="splitLine"></div>
+      <p>搜索詞：{{ searchTerm }}</p>
+      <p>類別：{{ category }}</p>
+    </div>
+  </main>
+</template>
+
+<script setup>
+import { ref } from "vue";
+import SearchInput from "./components/SearchInput.vue";
+
+const searchTerm = ref("");
+const category = ref("default");
+</script>
+
+<style>
+* {
+  box-sizing: border-box;
+  margin: 0;
+  padding: 0;
+  font-family: Arial, "PingFang SC", "Microsoft Yahei", sans-serif;
+}
+
+body {
+  background-color: #0f141c;
+  opacity: 1;
+  background-image: radial-gradient(
+    #212943 0.6000000000000001px,
+    #0f141c 0.6000000000000001px
+  );
+  background-size: 12px 12px;
+  color: white;
+}
+
+#app {
+  width: 100vw;
+  height: 100vh;
+  max-width: 100%;
+  display: grid;
+  place-items: center;
+}
+</style>
---
+++ composition-api-19-02-multiple-models/src/components/SearchInput.vue
@@ -0,0 +1,42 @@
+<template>
+  <label><span>搜索：</span><input type="text" v-model="searchModel" /> </label>
+  <label>
+    <span>類別：</span>
+    <select v-model="categoryModel">
+      <option value="default">預設</option>
+      <option value="fontend">前端</option>
+      <option value="backend">後端</option>
+      <option value="fullstack">全端</option>
+    </select>
+  </label>
+</template>
+
+<script setup>
+const searchModel = defineModel("searchModel");
+const categoryModel = defineModel("categoryModel");
+</script>
+
+<style scoped>
+label {
+  display: block;
+  margin-bottom: 20px;
+}
+
+input {
+  padding: 8px 14px;
+  border: 1px solid hsl(280deg, 50%, 50%);
+  border-radius: 4px;
+  outline: none;
+  background: hsl(280deg, 50%, 30%, 0.2);
+  color: white;
+}
+
+select {
+  border: 1px solid hsl(280deg, 50%, 50%);
+  background: none;
+  color: rgb(255, 255, 255);
+  padding: 6px;
+  border-radius: 4px;
+  background-color: #392e2e;
+}
+</style>
