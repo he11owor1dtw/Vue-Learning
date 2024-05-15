69894b86a5219f6f8180f76fd0371a11e0da4999
HelloCodeman <955khm@gmail.com>
2024-05-15 19:56:30 +0800

    Vue 配置代理：解決跨域問題

---
+++ vue-http-request-07-config-proxy/server.js
@@ -0,0 +1,48 @@
+const express = require("express");
+
+// create an express server with in-memory data to save notes
+const app = express();
+app.use(express.json());
+
+const notes = [
+  { id: 1, title: "筆記 1", content: "這是筆記 1" },
+  { id: 2, title: "筆記 2", content: "這是筆記 2" },
+  { id: 3, title: "筆記 3", content: "這是筆記 3" },
+  { id: 4, title: "筆記 4", content: "這是筆記 4" },
+];
+
+// enable CORS
+app.use(function (req, res, next) {
+  // 允許所有的跨域
+  // res.header("Access-Control-Allow-Origin", "*");
+  // 只允許同一個域名或同一個協議、端口
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, Authorization"
+  );
+  next();
+});
+
+// GET /notes?term=  search notes by content
+app.get("/notes", (req, res) => {
+  const term = req.query.term;
+  if (term) {
+    const filteredNotes = notes.filter((note) => note.content.includes(term));
+    res.json(filteredNotes);
+  } else {
+    res.json(notes);
+  }
+  // res.status(500).json({ message: "加載筆記列表出錯" });
+});
+
+app.post("/notes", (req, res) => {
+  const note = req.body;
+  note.id = notes.length + 1;
+  notes.push(note);
+  res.json(note);
+});
+
+// listen on port 3000
+app.listen(3000, () =>
+  console.log("Server listening on http://localhost:3000")
+);
---
+++ vue-http-request-07-config-proxy/vite.config.js
@@ -0,0 +1,24 @@
+import { fileURLToPath, URL } from "node:url";
+
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [vue()],
+  resolve: {
+    alias: {
+      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    },
+  },
+  server: {
+    proxy: {
+      // 統一發送到 /api 的請求，代理到 http://localhost:3000
+      "/api": {
+        target: "http://localhost:3000",
+        changeOrigin: true,
+        rewrite: (path) => path.replace(/^\/api/, ""),
+      },
+    },
+  },
+});
