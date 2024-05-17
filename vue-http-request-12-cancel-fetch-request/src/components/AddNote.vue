8ee57e107f7b7431828fba5734d8a925aa3825e2
HelloCodeman <955khm@gmail.com>
2024-05-17 08:30:52 +0800

    Vue Fetch 取消請求

---
+++ vue-http-request-12-cancel-fetch-request/src/components/AddNote.vue
@@ -0,0 +1,37 @@
+<template>
+  <div class="addNote">
+    <h2>添加新筆記</h2>
+    <form @submit.prevent="$emit('onNoteFormSubmit', { title, content })">
+      <input type="text" placeholder="請輸入筆記標題" v-model="title" />
+      <textarea
+        rows="6"
+        placeholder="請輸入筆記內容"
+        v-model="content"
+      ></textarea>
+      <button type="submit">添加筆記</button>
+    </form>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+const title = ref("");
+const content = ref("");
+
+defineEmits(["onNoteFormSubmit"]);
+</script>
+<style scoped>
+.addNote {
+  width: 100%;
+  border-top: 1px solid hsl(0, 0%, 95%);
+  padding-top: 24px;
+}
+
+.addNote h2 {
+  margin-bottom: 12px;
+}
+
+.addNote form {
+  display: grid;
+  gap: 24px;
+}
+</style>
