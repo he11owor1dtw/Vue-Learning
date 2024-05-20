6a22ddf3078af0254ca0e9f13c14ff20a273b1c2
HelloCodeman <955khm@gmail.com>
2024-05-21 01:59:22 +0800

    Vue 專案的打包

---
+++ vue-dev-build-deploy-02-build-vue-app/src/components/MessageList.vue
@@ -0,0 +1,65 @@
+<template>
+  <div>
+    <ul>
+      <MessageListItem
+        v-for="msg in messages"
+        :key="msg.id"
+        :id="msg.id"
+        :msg="msg.content"
+        @remove="removeMessage"
+      ></MessageListItem>
+    </ul>
+  </div>
+</template>
+<script setup>
+import { ref } from "vue";
+import MessageListItem from "./MessageListItem.vue";
+
+const messages = ref([
+  { id: 1, content: "這是一條消息提醒1" },
+  { id: 2, content: "這是一條消息提醒2" },
+  { id: 3, content: "這是一條消息提醒3" },
+  { id: 4, content: "這是一條消息提醒4" },
+]);
+
+function removeMessage(id) {
+  // console.log(id);
+  messages.value = messages.value.filter((msg) => msg.id !== id);
+}
+</script>
+<style scoped>
+div {
+  display: grid;
+  place-items: center;
+  gap: 24px;
+}
+
+h2 {
+  color: hsl(280deg, 50%, 70%);
+}
+
+p {
+  margin: 12px 0 24px 0;
+}
+
+ul {
+  list-style: none;
+  display: grid;
+  gap: 24px;
+}
+
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
---
+++ vue-dev-build-deploy-02-build-vue-app/src/components/MessageListItem.vue
@@ -0,0 +1,27 @@
+<template>
+  <li>
+    {{ msg }} <button @click="$emit('remove', id)">刪除</button>
+  </li>
+</template>
+<script setup>
+const props = defineProps(["msg", "id"]);
+const emits = defineEmits(["remove"]);
+
+// props 和 emits 可以在這裡使用，模板中可以繼續直接使用 props 屬性名，以及 this.$emit();
+</script>
+<style scoped>
+li {
+  display: flex;
+  align-items: center;
+  gap: 12px;
+}
+
+li::before {
+  content: "";
+  display: block;
+  width: 8px;
+  height: 8px;
+  background-color: hsl(280deg, 100%, 70%);
+  border-radius: 100%;
+}
+</style>
