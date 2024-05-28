42712c56cc47782f4c3cbc923279468d59e82f62
HelloCodeman <955khm@gmail.com>
2024-05-28 13:09:32 +0800

    Vue Vue Router Composition API

---
+++ vue-router-26-composition-api/src/components/ButtonLink.vue
@@ -0,0 +1,21 @@
+<template>
+  <button @click="navigate">
+    <slot />
+  </button>
+</template>
+<script>
+import { RouterLink, useLink } from "vue-router";
+export default {
+  props: {
+    ...RouterLink.props,
+    // 其它额外属性
+  },
+  setup(props) {
+    // 还有 route, href, isActive, isExactActive，与 router-link v-slot 暴露的 API 一致
+    const { navigate } = useLink(props);
+
+    return { navigate };
+  },
+};
+</script>
+<style scoped></style>
---
+++ vue-router-26-composition-api/src/pages/BlogListPage.vue
@@ -0,0 +1,45 @@
+<template>
+  <main>
+    <div className="blogPosts">
+      <article v-for="blogPost in blogPosts" :key="blogPost.id">
+        <h2>
+          <router-link :to="`/${blogPost.id}`">{{
+            blogPost.title
+          }}</router-link>
+        </h2>
+        <p>{{ blogPost.body.substring(0, 100) + "..." }}</p>
+      </article>
+    </div>
+  </main>
+</template>
+<script>
+import { getAllPosts } from "../data/blogPosts";
+import { ref } from "vue";
+export default {
+  setup() {
+    const blogPosts = ref([]);
+    blogPosts.value = getAllPosts();
+
+    return {
+      blogPosts,
+    };
+  },
+};
+</script>
+<style scoped>
+.blogPosts {
+  max-width: 600px;
+  display: grid;
+  row-gap: 48px;
+}
+
+h2 {
+  margin-bottom: 12px;
+  font-size: 32px;
+}
+
+h2 a {
+  color: hsl(280deg, 100%, 80%);
+  text-decoration: none;
+}
+</style>
